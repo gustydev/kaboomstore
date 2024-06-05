fddbe5af186a8e0e6bb676fd39ccd80530354f9e
gusty <gustavodiorlando@gmail.com>
2024-06-05 15:33:01 -0300

    Create css module for shop page

--- src/components/shop/Card.jsx
+++ src/components/shop/Card.jsx
@@ -1,9 +1,11 @@
+import styles from './shop.module.css';
+
 export default function Card( { title, image, price, rating } ) {
     return (
-        <div className="card">
-            <div className="title">{title}</div>
-            <img className="image" alt={title} src={image} style={{width: '100px', height: 'auto'}}></img>
-            <div className="price">{price}</div>
+        <div className={styles.card}>
+            <div className={styles.title}>{title}</div>
+            <img className={styles.image} alt={title} src={image}></img>
+            <div className={styles.price}>${price}</div>
             <div className="rating">{rating.rate}/5 ({rating.count})</div>
         </div>
     )
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -2,13 +2,15 @@ import { useState } from "react"
 import { useEffect } from "react"
 import Card from "./Card";
 import { useOutletContext } from "react-router-dom";
+import styles from './shop.module.css'
 
 export default function Shop() {
     const [data] = useOutletContext();
+
     if (data.length === 0) return 'Loading...'
 
     return (
-        <div className="card-container">
+        <div className={styles.cardContainer}>
             {data.map((i) => { return <Card key={i.id} title={i.title} image={i.image} price={i.price} rating={i.rating}></Card>})}
         </div>
     )
