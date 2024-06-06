4a261cbfc7da542917161442f8fa717d91f0ae67
gusty <gustavodiorlando@gmail.com>
2024-06-06 13:18:39 -0300

    Style home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
+import styles from './home.module.css';
 
-export default function Main() {
+export default function Home() {
     return (
-        <>
-        <div>Welcome to Kaboom! Here, we have the most explosive deals on the electronics market!</div>
-        <button><Link to='/shop'>Shop now</Link></button>
-        </>
+        <div className={styles.home}>
+            <div className={styles.messageContainer}>
+                <div className={styles.message}>Welcome to Kaboom! Here, we have the most explosive deals on the electronics market!</div>
+                <Link to='/shop' className={styles.button}><button>Shop now</button></Link>
+            </div>
+            <div className={styles.credits}>Photo by <a href="https://unsplash.com/pt-br/@alexkixa?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Alexandre Debiève</a> on <a href="https://unsplash.com/pt-br/fotografias/fotografia-macro-da-placa-de-circuito-preta-FO7JIlwjOtU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></div>
+        </div>
     )
 }
